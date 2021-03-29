8b13d1595e7ad11502f91b611e3c26627cbff29a
alienkrieg <alienkrieg@gmail.com>
2021-03-29 12:10:10 +0200

    minor changes

--- test/calculation.py
+++ test/calculation.py
@@ -1,14 +1,11 @@
 from context import iss
 import numpy as np
 
-X = np.array([1,3,2,8])
-Z = iss.get_increments(X)
+X = np.array([2,-4,3,7])
+Z = iss.get_increments(X, padding="left")
 
-print(f"X: {str(X)}")
-print(f"Z: {str(Z)}")
-
-comp = "[1]"
-ISS = iss.iterated_sums(Z, composition=comp)
+comp = "[1][11][1]"
+ISS = iss.iterated_sums(Z, composition=comp, verbose=True)
 
 print(f"<{comp},ISS(Z)> = {ISS}")
 print(f"ppv: {iss.ppv(ISS)}")
