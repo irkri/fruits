4c25b4ba1dd3749c675c4163ab5957d0c21986e6
alienkrieg <alienkrieg@gmail.com>
2021-04-17 19:01:36 +0200

    update new class names to test files

--- test/usage_example.py
+++ test/usage_example.py
@@ -2,7 +2,7 @@ from context import fruits
 import numpy as np
 
 # initialize FeatureExtractor
-featex = fruits.FeatureExtractor()
+featex = fruits.Fruit()
 
 np.random.seed(123)
 # initialize 1000 random time series of length 100 with 2 dimensions
@@ -16,7 +16,7 @@ print("prepared data; using identity function")
 print(featex.prepared_data()[0,0,:10])
 
 # try to use the increments of the time series as another DataPreparateur
-featex.add_data_preparateur(fruits.preparateurs.INC)
+featex.add(fruits.preparateurs.INC)
 
 print("\nprepared data; using increments function")
 print(featex.prepared_data()[0,0,:10])
@@ -26,10 +26,10 @@ sumit1 = fruits.SummationIterator.build_from_concatenationstring("[112][2212]")
 sumit2 = fruits.SummationIterator.build_from_concatenationstring("[12][111]")
 iterators = fruits.iterators.generate_random_concatenations(
 			number=8, dim=2, max_letter_weight=3, max_concatenation_length=5)
-featex.add_summation_iterator(sumit1)
-featex.add_summation_iterator(sumit2)
+featex.add(sumit1)
+featex.add(sumit2)
 for it in iterators:
-	featex.add_summation_iterator(it)
+	featex.add(it)
 
 print("\nUsed Concatenations:")
 print(sumit1)
@@ -43,9 +43,9 @@ print(f"\niterated sums; output shape with 10 SummationIterators: {iss.shape}")
 print(iss[0,0,:10])
 
 # add 3 features for testing purposes
-featex.add_feature(fruits.features.PPV)
-featex.add_feature(fruits.features.MAX)
-featex.add_feature(fruits.features.MIN)
+featex.add(fruits.features.PPV)
+featex.add(fruits.features.MAX)
+featex.add(fruits.features.MIN)
 
 # calculate features
 print(f"\nTotal number of features: {10}*{3}={10*3}")
