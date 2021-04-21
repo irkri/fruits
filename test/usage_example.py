c0bfc905b9f2242de9c05b3104b53a8c9500e7d9
alienkrieg <alienkrieg@gmail.com>
2021-04-22 00:14:40 +0200

    update example file

--- test/usage_example.py
+++ test/usage_example.py
@@ -1,55 +1,65 @@
 from context import fruits
 import numpy as np
 
-# initialize FeatureExtractor
+# initialize Fruit object
 featex = fruits.Fruit()
 
 np.random.seed(123)
 # initialize 1000 random time series of length 100 with 2 dimensions
-Y = np.random.rand(200000).reshape(1000, 2, 100)
+X = np.random.rand(200000).reshape(1000, 2, 100)
 
-# data preparation for FeatureExtractor
-featex.set_input_data(Y)
+# specify input data
+featex.set_input_data(X)
 
 # default preperator is the identity function
-print("prepared data; using identity function")
+print("prepared data; using identity function (i.e. doing nothing), "
+	  f"input shape: {X.shape}")
 print(featex.prepared_data()[0,0,:10])
 
 # try to use the increments of the time series as another DataPreparateur
 featex.add(fruits.preparateurs.INC)
 
-print("\nprepared data; using increments function")
+print(f"\nprepared data; using {fruits.preparateurs.INC}; "
+	  f"output shape: {featex.prepared_data().shape}")
 print(featex.prepared_data()[0,0,:10])
 
 # define SummationIterators to be used for the Iterated Sums
-sumit1 = fruits.SummationIterator.build_from_concatenationstring("[112][2212]")
-sumit2 = fruits.SummationIterator.build_from_concatenationstring("[12][111]")
-iterators = fruits.iterators.generate_random_concatenations(
-			number=8, dim=2, max_letter_weight=3, max_concatenation_length=5)
+# the simplest construct is a SimpleWord
+sumit1 = fruits.iterators.SimpleWord("[112][2212]")
+sumit2 = fruits.iterators.SimpleWord("[12][111]")
+
+# ... or let fruits choose random SimpleWords
+iterators = fruits.iterators.generate_random_words(
+				number=8, dim=2, monomial_length=3, n_monomials=5)
+
+# add function for the Fruit object can be used in many different ways
 featex.add(sumit1)
 featex.add(sumit2)
-for it in iterators:
-	featex.add(it)
+featex.add(iterators)
 
-print("\nUsed Concatenations:")
+print("\nUsed SummationIterators:")
 print(sumit1)
 print(sumit2)
 for it in iterators:
 	print(it)
 
-# calculate iterated sums
+# calculate iterated sums using the above defined SummationIterators
+# e.g. [<[112][2212], ISS(X, 0, N)> for N in range(len(X))]
 iss = featex.iterated_sums()
-print(f"\niterated sums; output shape with 10 SummationIterators: {iss.shape}")
+print(f"\niterated sums; output shape: {iss.shape}")
 print(iss[0,0,:10])
 
 # add 3 features for testing purposes
-featex.add(fruits.features.PPV)
+# PPV is tuneable, the quantile used to calculate this feature can be specified
+featex.add(fruits.features.PPV(quantile=0.75))
 featex.add(fruits.features.MAX)
 featex.add(fruits.features.MIN)
 
-# calculate features
-print(f"\nTotal number of features: {10}*{3}={10*3}")
+# use built-in methods for the Fruit object to get information about the 
+# processed data
+print(f"\nTotal number of features: {featex.nfeatures()}")
+
+# calculate the features
 features = featex.features()
-print(f"features; output shape using 3 different feature \
-functions: {features.shape}")
-print(features[:1,:])
+print(f"features; output shape: {features.shape}")
+print(features[0,:])
