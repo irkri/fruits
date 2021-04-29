1b12a1c640b313a0072e6a93250eb9f4d4fc7e53
alienkrieg <alienkrieg@gmail.com>
2021-04-30 01:28:06 +0200

    add numba support to available FeatureFilters

--- fruits/features.py
+++ fruits/features.py
@@ -1,6 +1,7 @@
 import numpy as np
 from copy import copy
 import collections
+import numba
 
 class FeatureFilter:
 	"""Class FeatureFilter
@@ -60,36 +61,49 @@ class FeatureFilter:
 		X = np.atleast_2d(X)
 		return self._func(X, *self._args, **self._kwargs)
 
+@numba.njit()
+def _fast_ppv(X:np.ndarray, ref_value:float) -> np.ndarray:
+	result = np.zeros(len(X))
+	for i in numba.prange(len(X)):
+		result[i] = np.sum(X[i]>=ref_value)/len(X[i])
+	return result
+
 def _ppv(X:np.ndarray,
 		 quantile:float=0.5,
 		 constant:bool=False,
-		 sample_size:int=0.05) -> np.ndarray:
+		 sample_size:float=0.05) -> np.ndarray:
 	if constant:
 		ref_value = quantile
 	else:
 		if not 0<quantile<1:
 			raise ValueError("If 'constant' is set to False, quantile has "
 							 "to be a value between 0 and 1")
-		selection = np.random.choice(np.arange(len(X)),
-									 size=int(sample_size*len(X)))
+		sample_size = int(sample_size*len(X))
+		sample_size = 1 if sample_size<1 else sample_size
+		selection = np.random.choice(np.arange(len(X)), size=sample_size)
 		ref_value = np.quantile(np.array([X[i] for i in selection]).flatten(),
 								quantile)
-	result = np.zeros(len(X))
-	for i in range(len(X)):
-		result[i] = np.sum(X[i]>=ref_value)/len(X[i])
-	return result
+	return _fast_ppv(X, ref_value)
 
 PPV = FeatureFilter("proportion of positive values")
 PPV.set_function(_ppv)
 
+@numba.njit()
 def _max(X:np.ndarray):
-	return np.max(X, axis=1)
+	result = np.zeros(len(X))
+	for i in numba.prange(len(X)):
+		result[i] = np.max(X[i])
+	return result
 
 MAX = FeatureFilter("maximal value")
 MAX.set_function(_max)
 
+@numba.njit()
 def _min(X:np.ndarray):
-	return np.min(X, axis=1)
+	result = np.zeros(len(X))
+	for i in numba.prange(len(X)):
+		result[i] = np.min(X[i])
+	return result
 
 MIN = FeatureFilter("minimal value")
 MIN.set_function(_min)
