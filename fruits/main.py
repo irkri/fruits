40096fd7f637ad45df752064145dbc60aca42be2
alienkrieg <alienkrieg@gmail.com>
2021-05-14 16:22:15 +0200

    feat: Fruit object is now a manager for a number of FruitBranch objects that have the same functionality as the original Fruit class

--- fruits/main.py
+++ fruits/main.py
@@ -8,7 +8,155 @@ from fruits.core import ISS
 class Fruit:
 	"""Feature Extractor using iterated sums.
 	
-	A Fruit object extracts values from time series data that are 
+	A Fruit object consists of a number of FruitBranch objects. At the
+	end of the pipeline, each branch returns their own features and
+	they will be concatenated by this class.
+	"""
+	def __init__(self, name:str=""):
+		# simple identifier for the Fruit object
+		self.name = name
+		# list of FruitBranches
+		self._branches = [FruitBranch()]
+		# pointer for the current branch
+		self._current_branch = self._branches[0]
+		# arrays of processed data for each branch
+		# if only one branch exists, these arrays will be three dimensional
+		# for multiple branches they are going to have four dimensions
+		self._prepared_data = None
+		self._iterated_data = None
+		self._sieved_data = None
+
+	@property
+	def name(self) -> str:
+		return self._name
+	
+	@name.setter
+	def name(self, name:str):
+		self._name = name
+
+	def add_branch(self, branch):
+		"""Adds a new branch to the pipeline.
+
+		:param branch: new branch
+		:type branch: FruitBranch
+		"""
+		self._branches.append(branch)
+
+	def branches(self) -> list:
+		"""Returns all branches of this Fruit object.
+		
+		:returns: list of branches
+		:rtype: {list}
+		"""
+		return self._branches
+
+	def switch_branch(self, index:int):
+		"""Switches to the branch with the given index. If the Fruit
+		objects points to a branch, then each method that is called on
+		the Fruit object is actually called on this branch.
+		
+		:param index: index between 0 and len(self.branches)
+		:type index: int
+		"""
+		self._current_branch = self._branches[index]
+
+	def nfeatures(self) -> int:
+		"""Returns the total number of features of all branches 
+		combined.
+		
+		:returns: number of features
+		:rtype: {int}
+		"""
+		return sum([branch.nfeatures() for branch in self._branches])
+
+	def add_data_preparateur(self, preparateur:DataPreparateur):
+		self._current_branch.add_data_preparateur(preparateur)
+
+	def get_data_preparateurs(self) -> list:
+		return self._current_branch.get_data_preparateurs()
+
+	def clear_data_preparateurs(self):
+		self._current_branch.clear_data_preparateurs()
+
+	def add_summation_iterator(self, sum_iter:SummationIterator):
+		self._current_branch.add_summation_iterator(sum_iter)
+
+	def get_summation_iterators(self) -> list:
+		return self._current_branch.get_summation_iterators()
+
+	def clear_summation_iterators(self):
+		self._current_branch.clear_summation_iterators()
+		
+	def add_feature_sieve(self, feat:FeatureSieve):
+		self._current_branch.add_feature_sieve(feat)
+
+	def get_feature_sieves(self) -> list:
+		return self._current_branch.get_feature_sieves()
+
+	def clear_feature_sieves(self):
+		self._current_branch.clear_feature_sieves()
+
+	def add(self, *objects):
+		self._current_branch.add(objects)
+
+	def clear(self):
+		self._current_branch.clear()
+
+	def set_input_data(self, X:np.ndarray):
+		for branch in self._branches:
+			branch.set_input_data(X)
+
+	def prepared_data(self) -> np.ndarray:
+		"""Returns the prepared data of the current branch.
+		
+		:returns: prepared data array
+		:rtype: {np.ndarray}
+		"""
+		return self._current_branch.prepared_data()
+
+	def iterated_sums(self) -> np.ndarray:
+		"""Returns the iterated sums of the current branch.
+		
+		:returns: three dimensional array of iterated sums
+		:rtype: {np.ndarray}
+		"""
+		return self._current_branch.iterated_sums()
+
+	def sieved_data(self):
+		"""Returns the features of the current branch.
+		
+		:returns: feature array
+		:rtype: {np.ndarray}
+		"""
+		return self._current_branch.features()
+
+	def features(self) -> np.ndarray:
+		"""Returns a two dimensional array of all features from all
+		branches this Fruit object contains.
+
+		:returns: two dimensional feature array
+		:rtype: {np.ndarray}
+		"""
+		if len(self._branches)==1:
+			return self._current_branch.features()
+		result = np.zeros((len(self._branches), self._current_branch._ts,
+			self.nfeatures()))
+		index = 0
+		for i, branch in enumerate(self._branches):
+			k = branch.nfeatures()
+			result[i, :, index:index+k] = branch.features()
+			index += k
+		return result
+
+	def __call__(self, X:np.ndarray) -> np.ndarray:
+		self.set_input_data(X)
+		return self.features()
+
+
+class FruitBranch:
+	"""One branch for a Fruit object.
+	
+	A FruitBranch object extracts values from time series data that are 
 	somehow representative of the input data.
 	The user can customize any of the following three steps the 
 	extractor is going to do in order to get the so called features.
@@ -33,9 +181,7 @@ class Fruit:
 	The number of features for one time series is equal to:: 
 		[number of iterators] x [number of sieves] 
 	"""
-	def __init__(self, name=""):
-		# simple identifier for the Fruit object
-		self.name = name
+	def __init__(self):
 		# used functions and class instances for data processing
 		# preparateurs will be called in the order that they're added
 		self._preparateurs = []
@@ -59,14 +205,6 @@ class Fruit:
 		self._ts_dim = 0
 		self._ts_length = 0
 
-	@property
-	def name(self) -> str:
-		return self._name
-	
-	@name.setter
-	def name(self, name:str):
-		self._name = name
-
 	def nfeatures(self) -> int:
 		"""Returns the total number of features the current
 		configuration produces.
@@ -84,7 +222,7 @@ class Fruit:
 		self._iterated = False
 		self._sieved = False
 
-	def get_data_preparateurs(self):
+	def get_data_preparateurs(self) -> list:
 		return self._preparateurs
 
 	def clear_data_preparateurs(self):
@@ -100,7 +238,7 @@ class Fruit:
 		self._iterated = False
 		self._sieved = False
 
-	def get_summation_iterators(self):
+	def get_summation_iterators(self) -> list:
 		return self._iterators
 
 	def clear_summation_iterators(self):
@@ -114,7 +252,7 @@ class Fruit:
 		self._sieves.append(feat)
 		self._sieved = False
 
-	def get_feature_sieves(self):
+	def get_feature_sieves(self) -> list:
 		return self._sieves
 
 	def clear_feature_sieves(self):
