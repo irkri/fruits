1a062f333dd6af28bde4db429093823999cee069
alienkrieg <alienkrieg@gmail.com>
2021-05-25 19:48:10 +0200

    upload setup.py for installing fruits locally

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name='fruits',
+        version='0.4.0',
+        author='alienkrieg',
+        author_email='alienkrieg@gmail.com',
+        description='Feature extRation Using ITerated Sums',
+        packages=['fruits'],
+        ext_package="",
+        ext_modules=[],
+        package_dir={'fruits':'fruits'},
+        py_modules=['fruits.__init__',
+                    'fruits.core',
+                    'fruits.features',
+                    'fruits.iterators',
+                    'fruits.preparateurs',
+                    'fruits.main'],
+        long_description=open('README.md').read(),
+        install_requires=[
+            "numpy >= 1.19.2",
+            "numba >= 0.52.0",
+            "pytest >= 6.2.4",
+        ],
+    )
