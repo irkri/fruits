928cd0ea28c452755032e562e1da09308b374ca9
alienkrieg <alienkrieg@gmail.com>
2021-05-30 16:16:14 +0200

    add tests for MAX, MIN and END with cut option enabled

--- tests/test_sieves.py
+++ tests/test_sieves.py
@@ -20,13 +20,34 @@ def test_ppv():
 
     np.testing.assert_allclose(ppv_1, ppv_1_copy)
 
-    ppv_c = fruits.features.PPVC(quantile=0, constant=True)
+    ppvc_1 = fruits.features.PPVC(quantile=0, constant=True)
 
-    np.testing.assert_allclose([0.4,0.4], ppv_c.fit_sieve(X_1[0]))
+    np.testing.assert_allclose([0.4,0.4], ppvc_1.fit_sieve(X_1[0]))
 
 def test_min_max():
-    maxi = fruits.features.MAX().fit_sieve(X_1[0])
-    mini = fruits.features.MIN().fit_sieve(X_1[1])
+    max_ = fruits.features.MAX().fit_sieve(X_1[0])
+    min_ = fruits.features.MIN().fit_sieve(X_1[1])
 
-    np.testing.assert_allclose(np.array([5,2]), maxi)
-    np.testing.assert_allclose(np.array([0,-8]), mini)
+    np.testing.assert_allclose(np.array([5,2]), max_)
+    np.testing.assert_allclose(np.array([0,-8]), min_)
+
+    max_cut_1 = fruits.features.MAX(cut=3).fit_sieve(X_1[0])
+    max_cut_2 = fruits.features.MAX(cut=0.5).fit_sieve(X_1[0])
+
+    np.testing.assert_allclose(np.array([0.8,2]), max_cut_1)
+    np.testing.assert_allclose(np.array([0.8,2]), max_cut_2)
+
+    min_cut_1 = fruits.features.MIN(cut=3).fit_sieve(X_1[0])
+    min_cut_2 = fruits.features.MIN(cut=0.5).fit_sieve(X_1[1])
+
+    np.testing.assert_allclose(np.array([-4,0]), min_cut_1)
+    np.testing.assert_allclose(np.array([5,-5]), min_cut_2)
+
+def test_end():
+    end = fruits.features.END().fit_sieve(X_1[0])
+
+    np.testing.assert_allclose(np.array([-3,-7]), end)
+
+    end_cut = fruits.features.END(cut=0.2).fit_sieve(X_1[0])
+
+    np.testing.assert_allclose(np.array([-4,2]), end_cut)
