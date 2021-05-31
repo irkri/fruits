efc62a818d9525c8f29a36e2f73dcc8d79af6ad6
alienkrieg <alienkrieg@gmail.com>
2021-06-01 00:51:35 +0200

    restructure setup file

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
 from setuptools import setup
 
+metadata = dict(
+    name='fruits',
+    version='0.5.0',
+    author='alienkrieg',
+    author_email='alienkrieg@gmail.com',
+    description='Feature extRation Using ITerated Sums',
+    packages=['fruits'],
+    ext_package="",
+    ext_modules=[],
+    py_modules=[
+                'fruits.__init__',
+                'fruits.core',
+                'fruits.features',
+                'fruits.iterators',
+                'fruits.preparateurs',
+                'fruits.main',
+                'fruits.accelerated',
+               ],
+    long_description=open('README.md').read(),
+    install_requires=[
+        "numpy >= 1.19.2",
+        "numba >= 0.52.0",
+        "pytest >= 6.2.4",
+    ],
+    python_requires=">=3.6, <3.10",
+)
+
 if __name__ == "__main__":
-    setup(
-        name='fruits',
-        version='0.5.0',
-        author='alienkrieg',
-        author_email='alienkrieg@gmail.com',
-        description='Feature extRation Using ITerated Sums',
-        packages=['fruits'],
-        ext_package="",
-        ext_modules=[],
-        package_dir={'fruits':'fruits'},
-        py_modules=[
-                    'fruits.__init__',
-                    'fruits.core',
-                    'fruits.features',
-                    'fruits.iterators',
-                    'fruits.preparateurs',
-                    'fruits.main',
-                    'fruits.accelerated',
-                   ],
-        long_description=open('README.md').read(),
-        install_requires=[
-            "numpy >= 1.19.2",
-            "numba >= 0.52.0",
-            "pytest >= 6.2.4",
-        ],
-    )
+    setup(**metadata)
