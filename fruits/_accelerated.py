56179c4c8ae641cb428b2c77eceb585b75ad0b8f
alienkrieg <alienkrieg@gmail.com>
2021-06-16 17:08:21 +0200

    remove scales from _fast_ISS

--- fruits/_accelerated.py
+++ fruits/_accelerated.py
@@ -3,23 +3,21 @@ import numpy as np
 
 @numba.njit(parallel=True, fastmath=True)
 def _fast_ISS(Z: np.ndarray, 
-              iterators: np.ndarray,
-              scales: np.ndarray) -> np.ndarray:
+              words: np.ndarray) -> np.ndarray:
     # accelerated function for calculation of
     # fruits.core.ISS(X, [SimpleWord(...)])
-    result = np.zeros((Z.shape[0], len(iterators), Z.shape[2]))
+    result = np.zeros((Z.shape[0], len(words), Z.shape[2]))
     for i in numba.prange(Z.shape[0]):
-        for j in numba.prange(len(iterators)):
+        for j in numba.prange(len(words)):
             result[i, j, :] = np.ones(Z.shape[2], dtype=np.float64)
-            for k in range(len(iterators[j])):
-                if not np.any(iterators[j][k]):
+            for k in range(len(words[j])):
+                if not np.any(words[j][k]):
                     continue
                 C = np.ones(Z.shape[2], dtype=np.float64)
-                for l in range(len(iterators[j][k])):
-                    if iterators[j][k][l] != 0:
-                        C = C * Z[i, l, :]**iterators[j][k][l]
+                for l in range(len(words[j][k])):
+                    if words[j][k][l] != 0:
+                        C = C * Z[i, l, :]**words[j][k][l]
                 result[i, j, :] = np.cumsum(result[i, j, :] * C)
-                result[i, j, :] /= Z.shape[2]**scales[j]
     return result
 
 @numba.njit(fastmath=True)
@@ -35,12 +33,12 @@ def _increments(X: np.ndarray):
                 result[i, j, k] = X[i, j, k] - X[i, j, k-1]
     return result
 
-@numba.njit
+@numba.njit()
 def _coquantile(X: np.ndarray, q: float):
     # calculates R = <[11], ISS(X)> and an index in R of the last
     # element less than or equal to q * max(R)
     # for q in (0, 1) the result is an integer in (0, len(X)]
     X_inc = _increments(np.expand_dims(np.expand_dims(X, axis=0), axis=1))
-    iss = _fast_ISS(X_inc, np.array([[[2]]]), np.array([0]))[0, 0, :]
+    iss = _fast_ISS(X_inc, np.array([[[2]]]))[0, 0, :]
     relative_max = iss[-1] * q
     return np.sum((iss <= relative_max))
