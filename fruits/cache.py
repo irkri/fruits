ffb8808efe9d60d1ae9ccc67710f53256d02a5a0
alienkrieg <alienkrieg@gmail.com>
2021-06-21 13:19:11 +0200

    method for get and process

--- fruits/cache.py
+++ fruits/cache.py
@@ -9,24 +9,27 @@ class FruitString:
 
         self._result = None
 
-    def get(self, X: np.ndarray = None) -> np.ndarray:
-        if X is None and self._result is not None:
-            return self._result
-        elif X is None and self._result is None:
-            raise RuntimeError("No results available, X has to be supplied")
-
+    def process(self, X: np.ndarray):
         self._result = X
         if self.preparateur is not None:
             self._result = self.preparateur.fit_prepare(self._result)
         if self.word is not None:
             self._result = ISS(self._result, self.word)[:, 0, :]
+
+    def get(self) -> np.ndarray:
+        if self._result is None:
+            raise RuntimeError("FruitString not processed")
         return self._result
 
     def is_empty(self) -> bool:
         if self.preparateur is None and self.word is None:
             return True
-        else:
-            return False
+        return False
+
+    def is_processed(self) -> bool:
+        if self._result is not None:
+            return True
+        return False
 
     def __eq__(self, other) -> bool:
         if not isinstance(other, FruitString):
