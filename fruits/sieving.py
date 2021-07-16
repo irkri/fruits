a10e3d1ad105ebb76d2a5ce16326506cdcd73eb9
alienkrieg <alienkrieg@gmail.com>
2021-07-16 16:03:10 +0200

    redo last commit

--- fruits/sieving.py
+++ fruits/sieving.py
@@ -367,19 +367,19 @@ class MAX(FeatureSieve):
         ``cut=[1,5,10]`` results in two features ``max(X[0:5])`` and
         ``max(X[4:10])``.
         If set to ``False``, then the left interval border is always 0.,
-        defaults to ``True``
+        defaults to ``False``
     :type segments: bool, optional
     :param name: Name of the object, defaults to "Maximal value"
     :type name: str, optional
     """
     def __init__(self,
                  cut: int = -1,
-                 segments: bool = True,
+                 segments: bool = False,
                  name: str = "Maximal value"):
         super().__init__(name)
         self._cut = cut if isinstance(cut, list) else [cut]
         if segments and len(self._cut) == 1:
-            raise ValueError("If 'segments' is set to False, then 'cut'"+
+            raise ValueError("If 'segments' is set to True, then 'cut'"+
                              " has to be a list length >= 2.")
         self._segments = segments
 
@@ -488,19 +488,19 @@ class MIN(FeatureSieve):
         ``cut=[1,5,10]`` results in two features ``min(X[0:5])`` and
         ``min(X[5:10])``.
         If set to ``False``, then the left interval border is always 0.,
-        defaults to ``True``
+        defaults to ``False``
     :type segments: bool, optional
     :param name: Name of the object, defaults to "Minimal value"
     :type name: str, optional
     """
     def __init__(self,
                  cut: int = -1,
-                 segments: bool = True,
+                 segments: bool = False,
                  name: str = "Minimum value"):
         super().__init__(name)
         self._cut = cut if isinstance(cut, list) else [cut]
         if segments and len(self._cut) == 1:
-            raise ValueError("If 'segments' is set to False, then 'cut'"+
+            raise ValueError("If 'segments' is set to True, then 'cut'"+
                              " has to be a list length >= 2.")
         self._segments = segments
 
