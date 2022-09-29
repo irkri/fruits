2105c821553cf4da87b523ebd9fa9b59ba4d4aa7
alienkrieg <alienkrieg@gmail.com>
2022-09-29 14:32:31 +0200

    change package version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = '2021, alienkrieg'
 author = 'alienkrieg'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.3'
+release = '0.10.1'
 
 
 # -- General configuration ---------------------------------------------------
