695c036ff390b31897d0fb07d7db605ef3d3fd7a
alienkrieg <alienkrieg@gmail.com>
2023-07-25 13:40:59 +0200

    fix import error

--- fruits/__init__.py
+++ fruits/__init__.py
@@ -1,5 +1,4 @@
 from . import cache, callback, iss, preparation, seed, sieving
-from .builder import build
 from .fruit import Fruit, FruitSlice
 from .iss import words, semiring
 from .iss.iss import ISS, ISSMode
